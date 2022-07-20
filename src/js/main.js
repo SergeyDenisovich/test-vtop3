5edf244201502f1467cb789982e5b5e405c7a127
Sergey Denisovich <densergej2016@gmail.com>
2022-07-20 16:26:07 +0300

    add scripts for animation

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,34 @@
 $(function () {
 	console.log("work");
+	// animate background svg
+	new Vivus(
+		"my-svg",
+		{
+			type: "delayed",
+			duration: 400,
+			animTimingFunction: Vivus.EASE,
+		},
+		function (obj) {
+			obj.el.classList.add("finished");
+		}
+	);
+	// animate fields
+	const fields = document.querySelectorAll(".animated");
+	for (let i = 0; i < fields.length; i++) {
+		let field = fields[i];
+		setTimeout(() => {
+			field.classList.remove("animated");
+			field.classList.add("visible");
+		}, i * 500);
+	}
+	// animate send button by click
+	const sendButton = document.querySelector("#sendBtn");
+	sendButton.addEventListener("click", shakeBtn);
+	function shakeBtn(e) {
+		e.preventDefault();
+		this.classList.add("shake");
+	}
+	sendButton.addEventListener("animationend", () => {
+		sendButton.classList.remove("shake");
+	});
 });
