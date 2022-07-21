7a9c07f61086a40c514d367b7cec591c9dd61959
Sergey Denisovich <densergej2016@gmail.com>
2022-07-21 23:40:31 +0300

    save changes

--- src/js/main.js
+++ src/js/main.js
@@ -152,6 +152,7 @@ $(function () {
 			const response = await fetch(
 				"https://jsonplaceholder.typicode.com/posts",
 				{
+					method: "POST",
 					body: JSON.stringify(formData),
 					headers: {
 						"Content-type": "application/json; charset=UTF-8",
